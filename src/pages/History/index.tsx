b16be8fd734fbfe611ba23bb6281d61a70b64faa
Vander Lima <vnd.vander@gmail.com>
2023-01-08 20:08:48 -0300

    feat: page history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,48 @@
-import { Header } from "../../components/Header"
+import { HistoryContainer, HistoryList } from "./styles";
 
 export function History() {
   return (
-    <div>
+    <HistoryContainer>
       <h1>History</h1>
-    </div>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }

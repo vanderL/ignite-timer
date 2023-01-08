2ed3934a2b3f3d8bbb427f34f74ceed1e31a8838
Vander Lima <vnd.vander@gmail.com>
2023-01-07 22:58:35 -0300

    feat: Styles LayoutDefault and Header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  nav {
+    display: flex;
+    gap: 0.5rem;
+
+    a {
+      width: 3rem;
+      height: 3rem;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      color: ${(props) => props.theme['gray-100']};
+
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+
+      transition: all ease 0.2s;
+
+      :hover {
+        border-bottom: 3px solid ${(props) => props.theme['green-500']};
+      }
+
+      &.active {
+        color: ${(props) => props.theme['green-500']}
+      }
+    }
+  }
+`;
---
+++ src/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,12 @@
+import { LayoutContainer } from './styles';
+import { Header } from '../../components/Header';
+import { Outlet } from 'react-router-dom';
+
+export function DefaultLayout() {
+  return (
+    <LayoutContainer>
+      <Header />
+      <Outlet />
+    </LayoutContainer>
+  )
+}

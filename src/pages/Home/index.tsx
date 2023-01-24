bcb0df4142bdb288df033388fbe65171e777c300
Vander Lima <vnd.vander@gmail.com>
2023-01-23 22:57:54 -0300

    feat: Init Countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Play } from 'phosphor-react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { differenceInSeconds } from 'date-fns';
 import * as zod from 'zod';
 
 import {
@@ -29,6 +30,7 @@ interface Cycle {
   id: string;
   task: string;
   minutesAmount: number;
+  startDate: Date;
 }
 
 export function Home() {
@@ -50,7 +52,8 @@ export function Home() {
     const newCycle: Cycle = {
       id,
       task: data.task,
-      minutesAmount: data.minutesAmount
+      minutesAmount: data.minutesAmount,
+      startDate: new Date()
     }
 
     setCycles(state => [...state, newCycle]);
@@ -71,6 +74,14 @@ export function Home() {
 
   const task = watch('task');
   const isSubmitDisabled = !task;
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate))
+      }, 1000)
+    }
+  }, [activeCycle])
 
   return (
     <HomeContainer>

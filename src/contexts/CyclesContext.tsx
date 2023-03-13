f24b02bda6787da444fd1e4a60a3d13a0422449f
Vander Lima <vnd.vander@gmail.com>
2023-03-13 20:06:51 -0300

    feat: Save object in Reducer

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -32,17 +32,44 @@ interface CyclesContextProviderProps {
   children: ReactNode
 }
 
+interface CyclesState {
+  cycles: Cycle[]
+  activeCycleId: string | null
+}
+
 export function CyclesContextProvider({ children }: CyclesContextProviderProps) {
-  const [cycles, dispatch] = useReducer((state: Cycle[], action: any) => {
+  const [cyclesState, dispatch] = useReducer((state: CyclesState, action: any) => {
     if (action.type === 'ADD_NEW_CYCLE') {
-      return [...state, action.payload.newCycle]
+      return {
+        ...state,
+        cycles: [...state.cycles, action.payload.newCycle],
+        activeCycleId: action.payload.newCycle.id
+      }
+    }
+
+    if (action.type === 'INTERRUPT_CURRENT_CYCLE') {
+      return {
+        ...state,
+        cycles: state.cycles.map(cycle => {
+          if (cycle.id === state.activeCycleId) {
+            return { ...cycle, interruptDate: new Date() }
+          } else {
+            return cycle
+          }
+        }),
+        activeCycleId: null
+      }
     }
 
     return state;
-  }, []);
+  }, {
+    cycles: [],
+    activeCycleId: null
+  });
 
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
+  const { cycles, activeCycleId } = cyclesState;
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
@@ -86,7 +113,6 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
       }
     })
     // setCycles(state => [...state, newCycle]);
-    setActiveCycleId(id);
     setAmountSecondsPassed(0);
   }
 
@@ -97,15 +123,6 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
         data: activeCycleId
       }
     })
-    // setCycles(state => state.map(cycle => {
-    //   if (cycle.id === activeCycleId) {
-    //     return { ...cycle, interruptDate: new Date() }
-    //   } else {
-    //     return cycle
-    //   }
-    // }),
-    // )
-    setActiveCycleId(null);
   }
 
   return (

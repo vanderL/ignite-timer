ef652a9df9a23d257621f8479e654963076e8a4f
Vander Lima <vnd.vander@gmail.com>
2023-03-14 00:11:48 -0300

    fix: Page - History - Correct date

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -26,7 +26,7 @@ export function History() {
               <tr key={cycle.id}>
                 <td>{cycle.task}</td>
                 <td>{cycle.minutesAmount} minutos</td>
-                <td>{formatDistanceToNow(cycle.startDate, {
+                <td>{formatDistanceToNow(new Date(cycle.startDate), {
                   addSuffix: true,
                   locale: ptBr,
                 })}</td>
